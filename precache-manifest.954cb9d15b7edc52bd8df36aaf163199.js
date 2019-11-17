99586d278f7c94d8103ad23bb4af38fe6de2caf1
lukaszbadocha <krakow@akademia108.pl>
2019-11-17 16:40:00 +0100

    Updates

---
+++ precache-manifest.954cb9d15b7edc52bd8df36aaf163199.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c4dc6d60f3f617c0f1fdadf038aec9",
+    "url": "/bitcoin-react/index.html"
+  },
+  {
+    "revision": "5c1143042aee47e7287f",
+    "url": "/bitcoin-react/static/css/main.7e318520.chunk.css"
+  },
+  {
+    "revision": "6c092804201548714bd0",
+    "url": "/bitcoin-react/static/js/2.5bc3d1e8.chunk.js"
+  },
+  {
+    "revision": "5c1143042aee47e7287f",
+    "url": "/bitcoin-react/static/js/main.25aa7cbb.chunk.js"
+  },
+  {
+    "revision": "29550aec70ae0ba7212c",
+    "url": "/bitcoin-react/static/js/runtime-main.99177cd7.js"
+  }
+]);
